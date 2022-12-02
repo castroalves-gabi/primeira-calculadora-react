7d98c9f56e01aea71e143f523a38f062d244cfd9
castroalves-gabi <ile.bgk@gmail.com>
2022-12-02 19:13:06 -0300

    css changes

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,15 +1,16 @@
 import styled from 'styled-components';
 
 export const ButtonContainer = styled.button`
-    padding: 20px;
-    border: 1px solid #CDCDCD;
-    background-color: #00AAF0;
+    padding: 40px;
+    border: 10;
+    border-radius: 25px;
+    background-color: #2a3447;
     color: #FFFFFF;
     font-size: 24px;
     font-weight: 700;
     flex: 1;
     
     &:hover {
-        opacity: 0.6;
+        opacity: 0.4;
     }
 `
--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,28 +1,26 @@
 import styled from 'styled-components';
 
 export const InputContainer = styled.div`
-    width: 100%;
-    height: 75px;
-    background-color: #AAAAFF;
-
+   
     display: flex;
     align-items: center;
     justify-content: flex-end;
 
-    font-size: 24px;
+    font-size: 100px;
     font-family: 'Roboto';
     
 
     input {
         width: 100%;
-        height: 75px;
-        background-color: #AAAAFF;
-        border: 0;
+        height: 120px;
+        background-color: #283345;
+        border: 10;
+        border-radius: 25px;
         display: flex;
         flex-direction: column;
         align-items: flex-end;
         padding: 0 10px;
-        font-size: 24px;
+        font-size: 40px;
         font-family: 'Roboto';
         color: #FFFFFF;
     }
